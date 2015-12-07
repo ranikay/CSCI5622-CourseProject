fc405cb533f1533346513a86901296d4c3cfe618
Nicolas Metts <N.Metts@cablelabs.com>
2015-12-07 16:32:45 -0700

    Added the weighted mean of precision, recall, and average, with recall weighted the highest, to the implementation of find_n_extreme_results. Including the results from running this with the top/bottom 20 and top/bottom 10.

--- code/find_n_extreme_results.py
+++ code/find_n_extreme_results.py
@@ -23,6 +23,14 @@ RECALL = 'Recall'
 ACCURACY = 'Accuracy'
 
 def __find_n_extreme_results(num_results, results_filename):
+    """
+    Finds the top/bottom n results for the metrics of accuracy, precision, and
+    recall and writes the results to a file. Also finds the top/bottom n
+    weighted average of all metrics.
+    Args:
+        num_results(int): The number of top/bottom results to find
+        results_filename: The name of the file containing all results
+    """
     results_file = open(results_filename)
     results_reader = csv.DictReader(results_file)
     field_names = results_reader.fieldnames
@@ -30,21 +38,31 @@ def __find_n_extreme_results(num_results, results_filename):
     precision_scores = []
     recall_scores = []
     all_rows = []
+    weighted_averages = []
     for row in results_reader:
         accuracy_scores.append(float(row[ACCURACY]))
         precision_scores.append(float(row[PRECISION]))
         recall_scores.append(float(row[RECALL]))
+        scores = np.array([float(row[ACCURACY]), float(row[PRECISION]),
+                           float(row[RECALL])])
+        weighted_mean = np.average(scores, weights=[0.25, 0.25, 1.0])
+        weighted_averages.append(weighted_mean)
         all_rows.append(row)
     results_file.close()
+    
+    # Cast the lists as numpy arrays
     accuracy_scores = np.array(accuracy_scores)
     precision_scores = np.array(precision_scores)
     recall_scores = np.array(recall_scores)
-    mean_accuracy = np.mean(accuracy_scores)
-    mean_precision = np.mean(precision_scores)
-    mean_recall = np.mean(recall_scores)
+    weighted_averages = np.array(weighted_averages)
+    
+    # Sort the arrays
     sorted_accuracy_scores = np.argsort(accuracy_scores)
     sorted_recall_scores = np.argsort(recall_scores)
     sorted_precision_scores = np.argsort(precision_scores)
+    sorted_averages = np.argsort(weighted_averages)
+    
+    # Find the top/bottom n scores for each metric
     top_n_accuracy_scores = sorted_accuracy_scores[len(accuracy_scores) - 1:
                                                    len(accuracy_scores) - num_results:
                                                    -1]
@@ -57,6 +75,12 @@ def __find_n_extreme_results(num_results, results_filename):
                                                    len(recall_scores) - num_results:
                                                    -1]
     bottom_n_recall_scores = sorted_recall_scores[0:num_results]
+    top_n_weighted_averages = sorted_averages[len(sorted_averages) - 1:
+                                              len(recall_scores) - num_results:
+                                              -1]
+    bottom_n_weighted_averages = sorted_averages[0:num_results]
+    
+    # Write the top/bottom results to file
     top_results_file = open('logs/top_' + str(num_results) + '_results.csv', 'w')
     bottom_results_file = open('logs/bottom_' + str(num_results) +'_results.csv', 'w')
     top_results_writer = csv.DictWriter(top_results_file, delimiter=',', 
@@ -65,16 +89,16 @@ def __find_n_extreme_results(num_results, results_filename):
                                            fieldnames=field_names)
     top_results_writer.writeheader()
     bottom_results_writer.writeheader()
-    #top_results_writer.writerow(field_names)
-    #bottom_results_writer.writerow(field_names)
     
-    # Combine any overlapping top results
+    # Combine any overlapping top results by creating a set of the indices
     all_top_results = set(np.concatenate((top_n_accuracy_scores,
                                           top_n_precision_scores,
-                                          top_n_recall_scores)))
+                                          top_n_recall_scores, 
+                                          top_n_weighted_averages)))
     all_bottom_results = set(np.concatenate((bottom_n_accuracy_scores,
                                           bottom_n_precision_scores,
-                                          bottom_n_recall_scores)))
+                                          bottom_n_recall_scores,
+                                          bottom_n_weighted_averages)))
     for i in all_top_results:
         # Don't include top results if accuracy, precision, or recall is 0
         if (float(all_rows[i][ACCURACY]) > 0.0 and 
